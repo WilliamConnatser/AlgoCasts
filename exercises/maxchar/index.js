9993e6a244df8e35359f1da5176968833e4aa836
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-03 22:55:54 -0500

    finished maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let max = str[0];
+
+    for (const char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+        if (charMap[char] > charMap[max]) max = char;
+    }
+
+    return max;
+}
 
 module.exports = maxChar;
