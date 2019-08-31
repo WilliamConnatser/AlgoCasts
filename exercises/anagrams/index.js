fc416d400f57844d7abf14aef36083987d296d14
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-08-31 11:52:07 -0500

    completed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const wordA = parseWord(stringA);
+    const wordB = parseWord(stringA);
+    if (wordA.keys !== wordB.keys) {
+        return false;
+    } else {
+        let valid = true;
+        Object.keys(wordA.count).forEach(key => {
+            console.log(wordA.count, wordB.count)         
+            if (wordA.count[key] !== wordB.count[key]) {
+                console.log(wordA.count[key], wordB.count[key])
+                valid = false;
+            }
+        })
+        return valid;
+    }
+}
+
+function parseWord(word) {
+    const preParse = word.replace(/[^\w]/g, '').toLowerCase();
+    const count = {};
+    let keys = 0;
+    for (const char of preParse) {
+        if (count[char]) count[char] + 1
+        else {
+            count[char] = 1;
+            keys++;
+        }
+    }
+    return {
+        keys,
+        count
+    }
+}
 
 module.exports = anagrams;
